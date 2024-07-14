4f2ebc8a33359c3ca99ed18a7b7eb6dd8bf5b215
Ivan Konderev <konderev@aheadworks.com>
2024-07-14 17:10:20 +0300

    Add dependences, store

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo, toggleTodo } from "./store";
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React + Redux Todo</h1>
+      <NewTodo />
+      <TodoList />
     </>
   )
 }
 
-export default App
+const NewTodo = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addTodo(event.target.title.value));
+    event.target.reset();
+  }
+  return <form onSubmit={handleSubmit}>
+    <input type="text" name="title" placeholder="New todo..." />
+    <input type="submit" value="Add Todo" />
+  </form>
+}
+
+const TodoList = () => {
+  const todos = useSelector(state => state);
+
+  return (
+    <ul>
+      {
+        todos.map(todo =>
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.completed} />
+            {todo.title}
+            <button>delete</button>
+          </li>
+        )
+      }
+    </ul>
+  )
+}
